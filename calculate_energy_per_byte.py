a9ca2dedc0c6efc7b8b54649907ab11fd4d09ddc
M. Ammar Izzudin <zoed.eldien15@gmail.com>
2025-10-18 20:48:50 +0700

    feat: Add commet step by step

--- calculate_energy_per_byte.py
+++ calculate_energy_per_byte.py
@@ -1,5 +1,6 @@
 # Simulate max distance between two nodes
 import pandas as pd
+import os
 
 from multihop.Packets import *
 from multihop.config import settings
@@ -11,8 +12,11 @@ energies = []
 energies_per_byte = []
 
 for size in sizes:
-    p = Message(MessageType.TYPE_ROUTED, 0, 0, 0, 1, [1] * size, None, [])
-    energy = preambles[settings.LORA_SF][settings.MEASURE_INTERVAL_S] * power_of_state(NodeState.STATE_PREAMBLE_TX) + p.time() * power_of_state(NodeState.STATE_TX)
+    # Fixed: Added settings as first argument, and proper values for all parameters
+    # Arguments: settings, msg_type, hops, lqi, dst, src, own_data, own_data_created_at, src_node, forwarded_msgs
+    p = Message(settings, MessageType.TYPE_ROUTED, 0, 0, 0, 1, [1] * size, 0, None, [])
+    # Fixed: power_of_state requires settings as first argument
+    energy = preambles[settings.LORA_SF][settings.MEASURE_INTERVAL_S] * power_of_state(settings, NodeState.STATE_PREAMBLE_TX) + p.time() * power_of_state(settings, NodeState.STATE_TX)
     energies.append(energy)
     energies_per_byte.append(energy/size)
 
@@ -21,5 +25,9 @@ df = pd.DataFrame({
     "energy": energies,
     "energy_per_byte": energies_per_byte
 })
+
+os.makedirs("results", exist_ok=True)
+filename = "./results/energy_per_byte_calculation.csv"
+df.to_csv(filename, index=False)
 
 print("The end")
